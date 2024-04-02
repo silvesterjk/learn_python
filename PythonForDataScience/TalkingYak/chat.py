87eb969a2150a8dce5c5ab3e5dab3862a458dbe3
silvesterjk <silvesterjkennedy@gmail.com>
2024-04-02 12:40:30 +0530

    Update chat.py

--- PythonForDataScience/TalkingYak/chat.py
+++ PythonForDataScience/TalkingYak/chat.py
@@ -9,28 +9,28 @@ from langchain.memory import ConversationSummaryBufferMemory
 from langchain.chat_models import ChatOpenAI
 
 # openai.api_key = os.environ["OPENAI_API_KEY"]
-TOGETHER_API_KEY = os.environ.get("TOGETHER_API_KEY")
+# TOGETHER_API_KEY = os.environ.get("TOGETHER_API_KEY")
 
-client = OpenAI(
-  api_key=TOGETHER_API_KEY,
-  base_url='https://api.together.xyz/v1',
-)
+# client = OpenAI(
+#   api_key=TOGETHER_API_KEY,
+#   base_url='https://api.together.xyz/v1',
+# )
 
-chat_completion = client.chat.completions.create(
-  messages=[
-    {
-      "role": "system",
-      "content": "You are an expert travel guide.",
-    },
-    {
-      "role": "user",
-      "content": "Tell me fun things to do in San Francisco.",
-    }
-  ],
-  model="mistralai/Mixtral-8x7B-Instruct-v0.1"
-)
+# chat_completion = client.chat.completions.create(
+#   messages=[
+#     {
+#       "role": "system",
+#       "content": "You are an expert travel guide.",
+#     },
+#     {
+#       "role": "user",
+#       "content": "Tell me fun things to do in San Francisco.",
+#     }
+#   ],
+#   model="mistralai/Mixtral-8x7B-Instruct-v0.1"
+# )
 
-print(chat_completion.choices[0].message.content)
+# print(chat_completion.choices[0].message.content)
 
 memory = ConversationSummaryBufferMemory(llm=ChatOpenAI(), max_token_limit=2048)
 conversation = ConversationChain(
