5cf63b16fb8e87b0e8a4afb029611ad18fd8ede7
silvesterjk <silvesterjkennedy@gmail.com>
2024-04-05 13:37:12 +0530

    5thApr-1

---
+++ PythonForDataScience/Langchain/Chatbot/Localama.py
@@ -0,0 +1,14 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+## Prompt Template
