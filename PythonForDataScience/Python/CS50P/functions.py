b6b1f522400e1bb8d6947ab940a7e504ef67ba72
silvesterjk <silvesterjkennedy@gmail.com>
2024-05-09 08:41:01 +0530

    1

--- PythonForDataScience/Python/CS50P/functions.py
+++ PythonForDataScience/Python/CS50P/functions.py
@@ -1,1 +1,21 @@
 #%%
+print("Hello Nicy")
+#%%
+# Arguments are input that a function can take
+# They are also referred to as parameters passed on to the function
+print("Hello, \"nicy\"")
+# %%
+name = "Hello Nicy"
+# %%
+name = "Hello Nicy             "
+hello_nicy = name.strip()
+print(hello_nicy)
+# %%
+#Calculator
+x = int(input("What is x? : "))
+y = int(input("What is y? : "))
+
+z = x +y
+
+print(z)
+# %%
