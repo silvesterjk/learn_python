7425ada8041352fd4a0f436dca7d232f125e960f
silvesterjk <silvesterjkennedy@gmail.com>
2025-01-10 15:47:10 +0530

    10/1 - 2nd

--- oop/10_car.py
+++ oop/10_car.py
@@ -9,10 +9,15 @@ print(vars(car3))
 
 # The __dict__ attribute is a dictionary that contains the object's attributes.
 # The vars() function is a built-in function that returns the __dict__ attribute of an object.
+# __str__ can be called using the print() function. For example, print(car1) will call the __str__ method of the Car class.
 print(car1)
 print(car1.drive())
 print(car1.description())
 
 
 # Class variables are shared among all instances of a class. They are defined outside the __init__ method.
-# __str__ can be called using the print() function. For example, print(car1) will call the __str__ method of the Car class.
+# Instance variables are unique to each instance of a class. They are defined inside the __init__ method.
+# Class variables are accessed using the class name. For example, Car.wheels.
+# Instance variables are accessed using the self keyword. For example, self.model.
+# Inheritance is a way to create a new class from an existing class. The new class is called a subclass, and the existing class is called a superclass.
+
