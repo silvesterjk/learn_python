2cba02f4a5dabfca60cf484c26698a853eafe534
silvesterjk <silvesterjkennedy@gmail.com>
2025-01-18 12:34:55 +0530

    18/1 - 3rd

---
+++ oop/17_nested.py
@@ -0,0 +1,12 @@
+# Nested classed = A class defined within another class
+"""
+class Outer:
+    class Inner:
+        pass
+"""
+
+# Benefits of nested classes:
+# 1. Namespacing: Nested classes can be used to group related classes together.
+# 2. Encapsulation: Nested classes can be used to hide classes that are only used by the outer class. And allows private access to the inner class.
+# 3. Readability: Nested classes can be used to improve the readability of the code.
+
