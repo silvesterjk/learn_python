36de8ad969a87b91a4005103d688e82ae476ceda
silvesterjk <silvesterjkennedy@gmail.com>
2025-01-19 14:20:45 +0530

    19/1 - 2nd

--- syntax/8.oop.py
+++ syntax/8.oop.py
@@ -5,3 +5,35 @@ This approach promotes modularity, reusability, and organization in software dev
 
 """2. Defining Classes and Creating Objects:
 A class serves as a blueprint for creating objects, defining the attributes and methods common to all objects of that type. An object is an instance of a class."""
+
+# Here, Student is a class with an initializer method __init__ that sets the name and house attributes for each student object.
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+"""3. Creating and Using Objects
+
+Once a class is defined, objects can be instantiated and their attributes accessed or modified."""
+
+student = Student("Harry", "Gryffindor")
+print(student.name)  # Outputs: Harry
+print(student.house)  # Outputs: Gryffindor
+
+"""4. Adding Methods to Classes
+
+Methods are functions defined within a class that describe the behaviors of an object."""
+
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+    def introduce(self):
+        return f"My name is {self.name} and I belong to {self.house} house."
+
+# Now, each Student object has an introduce method:
+
+student = Student("Hermione", "Gryffindor")
+print(student.introduce())  # Outputs: My name is Hermione and I belong to Gryffindor house.
+
