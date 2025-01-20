204e91f34f296fe975b6c6814248fd0fc5be97d0
silvesterjk <silvesterjkennedy@gmail.com>
2025-01-20 18:11:30 +0530

    20/1 - 2nd

--- syntax/9.et_cetera.py
+++ syntax/9.et_cetera.py
@@ -133,5 +133,6 @@ print(squared_dict)
 for index, value in enumerate(numbers):
     print(f"Index: {index}, Value: {value}")
 
+# Output:
 for index, value in enumerate(numbers, start=1):
     print(f"Index: {index}, Value: {value}")
