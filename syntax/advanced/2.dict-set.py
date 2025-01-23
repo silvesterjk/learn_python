c150e856f69e71b7370cb95a00835f5d337a064e
silvesterjk <silvesterjkennedy@gmail.com>
2025-01-23 20:40:50 +0530

    Update 2.dict-set.py

--- syntax/advanced/2.dict-set.py
+++ syntax/advanced/2.dict-set.py
@@ -6,8 +6,8 @@ print(mydict)
 value = mydict["name"]
 print(value) # John
 
-mydict["lastname"]
-print(mydict) # KeyError: 'lastname'
+# mydict["lastname"]
+# print(mydict) # KeyError: 'lastname'
 
 mydict["lastname"] = "Doe"
 print(mydict) # {'name': 'John', 'age': 36, 'country': 'Norway', 'city': 'Oslo', 'lastname': 'Doe'}
@@ -24,10 +24,10 @@ else:
 
 print(len(mydict)) # 3
 
-try:
-    print(mydict["supername"])
-except KeyError:
-    print("KeyError: 'supername'")
+# try:
+#     print(mydict["supername"])
+# except KeyError:
+#     print("KeyError: 'supername'")
 
 for value in mydict.values():
     print(value) # John 36 Norway
@@ -59,8 +59,8 @@ mydict5 = {4: "apple", 5: "banana"}
 print(mydict5)  # {1: 'apple', 2: 'banana'}
 print(mydict5[4])  # apple -- 4 instead of 0 because the key is 4
 
-mydict6 = {["name", "age"]: "John"}  # TypeError: unhashable type: 'list' 
 # List is mutable and cannot be used as a key in a dictionary
+# mydict6 = {["name", "age"]: "John"}  # TypeError: unhashable type: 'list' 
 
 """SET"""
 
@@ -85,3 +85,47 @@ print(myset)  # {'orange', 'banana', 'apple', 'mango', 'grapes', 'cherry'}
 
 # An empty set cannot be created using {}
 myset3 = set() # create an empty set
+
+myset3.add(1)
+myset3.add(2)
+myset3.add(3)
+myset3.add(4)
+print(myset3)  # {1, 2, 3, 4}
+
+myset3.remove(2) # remove 2 from the set
+print(myset3)  # {1, 3, 4}
+
+myset3.discard(3) # remove 3 from the set
+print(myset3)  # {1, 4}
+
+myset3.pop() # removes an arbitrary element from the set
+print(myset3) # {4} -- 1 is removed
+
+myset3.clear() # clear the set
+
+for x in myset:
+    print(x)  # orange banana apple mango grapes cherry
+
+if "apple" in myset:
+    print("yes")
+else:  
+    print("no")
+
+# Union of two sets
+set1 = {1, 2, 3}
+set2 = {4, 5, 6}
+set3 = set1.union(set2)
+print(set3)  # {1, 2, 3, 4, 5, 6}
+
+
+# Intersection of two sets
+set4 = set1.intersection(set2)
+print(set4)  # set()
+
+# Difference of two sets
+set5 = set1.difference(set2)
+print(set5)  # {1, 2, 3} --> set1 - set2
+
+# Symmetric difference of two sets
+set6 = set1.symmetric_difference(set2)
+print(set6)  # {1, 2, 3, 4, 5, 6} --> (set1 - set2) U (set2 - set1)
