9416f80997d68872f7afd95e17348a686f40130c
silvesterjk <silvesterjkennedy@gmail.com>
2025-01-24 17:11:07 +0530

    Update 3.strings-collections.py

---
+++ syntax/advanced/3.strings-collections.py
@@ -0,0 +1,10 @@
+"""STRING"""
+# Strings are immutable sequences of Unicode code points.
+# Strings are arrays of bytes representing Unicode characters.
+
+mystring = "Hello, World!"
+mystring2 = 'Hello, World!'
+
+print(mystring)
+print(mystring2)
+
