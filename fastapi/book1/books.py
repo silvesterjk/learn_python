6cf76dac703bd409b233600d892c193a34643475
silvesterjk <silvesterjkennedy@gmail.com>
2025-01-26 19:42:13 +0530

    Update books.py

--- fastapi/book1/books.py
+++ fastapi/book1/books.py
@@ -74,3 +74,5 @@ async def delete_book(book_title: str):
         if BOOKS[i].get('title').casefold() == book_title.casefold():
             BOOKS.pop(i)
             break
+
+@app.put("/books/update_book/{book_title}")
