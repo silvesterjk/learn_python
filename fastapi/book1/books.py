030a12a231619c694ca97d1db3a91dd5a8c73813
silvesterjk <silvesterjkennedy@gmail.com>
2025-01-28 15:20:40 +0530

    Update books.py

--- fastapi/book1/books.py
+++ fastapi/book1/books.py
@@ -74,8 +74,3 @@ async def delete_book(book_title: str):
         if BOOKS[i].get('title').casefold() == book_title.casefold():
             BOOKS.pop(i)
             break
-
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host=5000)
