b62a1def128d58bd46139dd95081ebb702803127
Silvester J Kennedy <108580657+silvesterjk@users.noreply.github.com>
2025-02-12 18:21:20 +0530

    Update 3.strings-collections.py

--- syntax/advanced/3.strings-collections.py
+++ syntax/advanced/3.strings-collections.py
@@ -4,7 +4,13 @@
 
 mystring = "Hello, World!"
 mystring2 = 'Hello, World!'
+mystring3 = 'Hello, \
+    World!'
+
+mystring4 = """Hello, 
+    World!"""
 
 print(mystring)
 print(mystring2)
-
+print(mystring3)
+print(mystring4)
