26e69ea74dd8947e2245934ab84fae8e52512f4e
Silvester J Kennedy <108580657+silvesterjk@users.noreply.github.com>
2025-02-12 20:36:26 +0530

    Update 3.strings-collections.py

--- syntax/advanced/3.strings-collections.py
+++ syntax/advanced/3.strings-collections.py
@@ -14,3 +14,22 @@ print(mystring)
 print(mystring2)
 print(mystring3)
 print(mystring4)
+
+mystring5 = "Hello, World!"
+mystring5[0] = 'h' # TypeError: 'str' object does not support item assignment
+char = mystring5[0]
+print(char) # H
+
+range_ = mystring5[0:5]
+print(range_) # Hello
+
+range_step2 = mystring5[0:5:2]
+print(range_step2) # Hlo
+
+step3 = mystring5[::3]
+print(step3) # HlWl
+
+step3rev = mystring5[::-1]
+print(step3rev) # !dlroW ,olleH
+
+# String concatenation
