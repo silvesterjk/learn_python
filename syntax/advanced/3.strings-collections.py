f0cdbb845563ce1e6aabd571826975ea21da9d06
Silvester J Kennedy <108580657+silvesterjk@users.noreply.github.com>
2025-02-17 13:10:16 +0530

    Update 3.strings-collections.py

--- syntax/advanced/3.strings-collections.py
+++ syntax/advanced/3.strings-collections.py
@@ -50,3 +50,5 @@ print(formatted_string) # My name is John and I am 25 years old
 
 formatted_string2 = "My name is {0} and I am {1} years old".format(name, age)
 print(formatted_string2) # My name is John and I am 25 years old
+
+formatted_string3 = "My name is {name} and I am {age} years old".format(name="John", age=25)
