a245f1f5a846139bd3e030acbc0db856c1eb0ffc
Silvester J Kennedy <108580657+silvesterjk@users.noreply.github.com>
2025-02-24 20:54:43 +0530

    Update 9.decorator-generator.py

--- syntax/advanced/9.decorator-generator.py
+++ syntax/advanced/9.decorator-generator.py
@@ -1,4 +1,8 @@
 # DECORATORS
 
 def my_decorator(func):
-    ...
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
