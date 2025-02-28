6f402b0a5b1348bd885335a83a0c0da4c53b981d
Silvester J Kennedy <108580657+silvesterjk@users.noreply.github.com>
2025-02-28 17:08:39 +0530

    Update books.py

--- fastapi/book1/books.py
+++ fastapi/book1/books.py
@@ -34,14 +34,17 @@ async def read_all_books():
 # Get all books from a specific category using path parameters
 
 @app.get("/books")
-async def read_book()
+async def read_book():
     return BOOKS
 
-@app.get("/books/{dynamic_parameter}")
-async def read_books(dynamic_parameter: str):
+@app.get("/parama_books/{dynamic_parameter}")
+async def parama_read_books(dynamic_parameter: str):
     return {'books_to_return': dynamic_parameter}
 
 # ___________________________________________________
+
+# The order of the path parameters matters
+# Get all books from a specific category and author using path parameters
 
 @app.get("/books/category/{category}")
 async def read_category(category: str):
@@ -57,66 +60,29 @@ async def read_category(category: str):
             books_to_return.append(book)
     return books_to_return
 
-"""
-# The order of the path parameters matters
-# Get all books from a specific category and author using path parameters
+
 @app.get("/books/category/horror")
 async def read_category():
     return {"data": "Horror"}
 # ^ The above path will not work because the path parameters are not in the correct order. 
 # @app.get("/books/category/{category}") will not work because the path parameters are not in the correct order.
-"""
+
 # %20 is the URL encoding for a space character
 # http://0.0.0.0:8000/books/category/science/author/Author%20One -> This is the URL
 # science -> This is the category path parameter
 # Author One -> This is the author path parameter
 
-# Get all books from a specific author using path or query parameters
-@app.get("/books/byauthor/")
-async def read_books_by_author_path(author: str):
+# Get all books from a specific category and author using path parameters
+@app.get("/books/category/{category}/author/{author}")
+async def read_category_author(category: str, author: str):
+    """
+    1. The path parameters are the category and the author
+    2. The path parameters are passed to the function as arguments
+    3. The function returns all the books with the category and author that match the path parameters
+    4. The function returns a list of books with the category and author that match the path parameters
+    """
     books_to_return = []
     for book in BOOKS:
-        if book.get('author').casefold() == author.casefold():
+        if book.get('category').casefold() == category.casefold() and book.get('author').casefold() == author.casefold():
             books_to_return.append(book)
-
     return books_to_return
-
-
-@app.get("/books/{book_author}/")
-async def read_author_category_by_query(book_author: str, category: str):
-    books_to_return = []
-    for book in BOOKS:
-        if book.get('author').casefold() == book_author.casefold() and \
-                book.get('category').casefold() == category.casefold():
-            books_to_return.append(book)
-
-    return books_to_return
-
-
-@app.post("/books/create_book")
-async def create_book(new_book=Body()):
-    BOOKS.append(new_book)
-
-
-@app.put("/books/update_book")
-async def update_book(updated_book=Body()):
-    for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
-            BOOKS[i] = updated_book
-
-
-@app.delete("/books/delete_book/{book_title}")
-async def delete_book(book_title: str):
-    for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == book_title.casefold():
-            BOOKS.pop(i)
-            break
-
-
-@app.delete("/books/delete_books/")
-async def delete_books_by_category(category: str):
-    for i in range(len(BOOKS)):
-        if BOOKS[i].get('category').casefold() == category.casefold():
-            BOOKS.pop(i)
-            break
-            
