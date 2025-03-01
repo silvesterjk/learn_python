292486065cdfded11cfb4259049e3c18ba3454f4
Silvester J Kennedy <108580657+silvesterjk@users.noreply.github.com>
2025-03-01 21:51:55 +0530

    Update books.py

--- fastapi/book1/books.py
+++ fastapi/book1/books.py
@@ -215,3 +215,8 @@ async def delete_book_path(title: str):
             print(f'"Book {title} has been deleted."')
             return b
     return {"data": "Book not found."}
+
+"""
+Payload:
+{
+"""
