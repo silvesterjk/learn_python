782393954a4df2b85627e487a8b3687a7db235a3
silvesterjk <silvesterjkennedy@gmail.com>
2025-03-06 18:56:18 +0530

    Update books2.py

--- fastapi/books/books2.py
+++ fastapi/books/books2.py
@@ -6,4 +6,14 @@
 5. Request Objects
 """
 
+
+"""
+HTTP Request Methods: Comparision between CRUD and HTTP Request Methods
+1. GET: To request data from the server. This is similar to the Read operation in CRUD.
+2. POST: To submit data to the server. This is similar to the Create operation in CRUD.
+3. PUT: To update data on the server. This is similar to the Update operation in CRUD.
+4. DELETE: To delete data from the server. This is similar to the Delete operation in CRUD.
+"""
+
 from fastapi import FastAPI, Body, HTTPException
+from typing import Optional, List
