92eb45db7b0d638b858fdd45c3ffb62131af2f07
silvesterjk <silvesterjkennedy@gmail.com>
2025-03-06 21:12:48 +0530

    Update books2.py

--- fastapi/books/books2.py
+++ fastapi/books/books2.py
@@ -20,25 +20,45 @@ Pydantic 1 vs Pydantic 2
 Pydantic 2 has made some changes to the library. The three biggest are:
 * .dict() function is now renamed to .model_dump()
 * schema_extra function within a Config class is now renamed to json_schema_extra
-* Optional variables need a =None example: id: Optional[int] = None
+* Optional variables need a = None --> For example: id: Optional[int] = None
 """
 
 from fastapi import FastAPI, Body, HTTPException
 from typing import Optional, List
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class BookRequest(BaseModel):
+    id: int
+    title: str 
+    author: str
+    description: Optional[str] = None
+    category: str
+
+    # Constructor
+    def __init__(self, id, title, author, description=None, category=None):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.category = category
+
 # Create a list of books
 BOOKS = [
-    {"title": "Title One", "author": "Author One", "category": "history"},
-    {"title": "Title Two", "author": "Author Two", "category": "fiction"},
-    {"title": "Title Three", "author": "Author Three", "category": "science"},
-    {"title": "Title Four", "author": "Author Four", "category": "biography"},
-    {"title": "Title Five", "author": "Author Five", "category": "history"},
-    {"title": "Title Six", "author": "Author Six", "category": "fiction"},
-    {"title": "Title Seven", "author": "Author Seven", "category": "science"}
+    BookRequest(id=1, title="Title One", author="Author One", description="Amazing work",category="history"),
+    BookRequest(id=2, title="Title Two", author="Author Two", category="science"),
+    BookRequest(id=3, title="Title Three", author="Author Three", category="fiction"),
+    BookRequest(id=4, title="Title Four", author="Author Four", category="history"),
+    BookRequest(id=5, title="Title Five", author="Author Five", category="science"),
+    BookRequest(id=6, title="Title Six", author="Author Six", category="fiction"),
+    BookRequest(7, "Title Seven", "Author Seven", "Great work", "history")
 ]
 
 @app.get("/get_books")
 async def get_all_books():
     return BOOKS
+
+@app.post("/get_books/create_book")
+async def create_book(book_request = Body()): # Instead of Book = Body(...) we could also use Body() with a type hint
+    BOOKS.append(book_request)
