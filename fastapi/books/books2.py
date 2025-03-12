b988a984995914f1ee900e0a9e64b512e54fbb08
silvesterjk <silvesterjkennedy@gmail.com>
2025-03-12 21:23:16 +0530

    Update books2.py

--- fastapi/books/books2.py
+++ fastapi/books/books2.py
@@ -229,4 +229,5 @@ Request Body: The request body is used to send data to the server.
 2. The request body is passed to the function as an argument.
 3. The request body is used to create new resources.
 4. The request body is used to update existing resources.
+5. The request body is used to delete resources.
 """
