c9d57ee1d3c45ed9a38f255332128bffa0c36dde
silvesterjk <silvesterjkennedy@gmail.com>
2025-03-12 21:22:55 +0530

    Update books2.py

--- fastapi/books/books2.py
+++ fastapi/books/books2.py
@@ -227,4 +227,5 @@ Query Parameters: Query parameters are used to pass data to the server using the
 Request Body: The request body is used to send data to the server.
 1. The request body is the data sent to the server.
 2. The request body is passed to the function as an argument.
+3. The request body is used to create new resources.
 """
