9e52d8213c28c9890cd0f6273760d91084e5b5cc
Silvester J Kennedy <108580657+silvesterjk@users.noreply.github.com>
2025-04-15 13:01:57 +0530

    Update books3.py

--- fastapi/books/books3.py
+++ fastapi/books/books3.py
@@ -8,8 +8,3 @@ app = FastAPI()
 
 # Database connection
 def get_db_connection():
-    conn = sqlite3.connect('books.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-# Pydantic model for Book
