6d43f716ae55ff5fa1fecdbb3e61450062f073d7
Silvester J Kennedy <108580657+silvesterjk@users.noreply.github.com>
2025-04-16 15:10:03 +0530

    Update books3.py

--- fastapi/books/books3.py
+++ fastapi/books/books3.py
@@ -11,4 +11,17 @@ def get_db_connection():
     conn = sqlite3.connect('books.db')
     conn.row_factory = sqlite3.Row
     return conn
+
 # Create the table if it doesn't exist
+def create_table():
+    conn = get_db_connection()
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS books (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            author TEXT NOT NULL,
+            published_date TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
