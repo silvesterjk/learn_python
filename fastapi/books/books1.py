cbad1e6b8ca793b2a26527ef4d707989b42717c7
Silvester J Kennedy <108580657+silvesterjk@users.noreply.github.com>
2025-04-27 21:57:39 +0530

    Update books1.py

--- fastapi/books/books1.py
+++ fastapi/books/books1.py
@@ -225,3 +225,4 @@ Example Request Body:
 
 # ___________________________________________________
 # Get all books from a specific category and author using query parameters
+@app.get("/books/category/{category}/author/{author}")
