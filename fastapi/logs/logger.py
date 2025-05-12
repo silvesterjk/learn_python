3b46d1943abf2d164618ad10ae8dbe98153ca576
Silvester J Kennedy <108580657+silvesterjk@users.noreply.github.com>
2025-05-12 20:01:05 +0530

    Update logger.py

--- fastapi/logs/logger.py
+++ fastapi/logs/logger.py
@@ -25,6 +25,3 @@ The logs would get printed like this:
 handler = logging.StreamHandler()
 filer_handler = logging.Filter("__main__")
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-handler.setFormatter(formatter)
-logger.addHandler(handler
