6303b4f76d59bd68c8e0969ea532bb06d24c7e04
Silvester J Kennedy <108580657+silvesterjk@users.noreply.github.com>
2025-05-24 15:21:28 +0530

    Update database.py

--- fastapi/eric-roby/Project 3/TodoApp/database.py
+++ fastapi/eric-roby/Project 3/TodoApp/database.py
@@ -9,5 +9,3 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
